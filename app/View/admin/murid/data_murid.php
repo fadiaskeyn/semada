fbe4555744a47191838902cb858a0ab847a1db5a
Kurniawan <fajard916@gmail.com>
2023-12-01 00:31:32 +0700

    Fix Branch View Tabrakan sama Master

--- app/View/admin/murid/data_murid.php
+++ app/View/admin/murid/data_murid.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
   <head>
     <title>Data Murid</title>
     <meta charset="UTF-8">
@@ -287,56 +286,4 @@
     <script src="../../../../asset/js/search.js"></script>
     <script src="../../../../asset/js/scripts.js"></script>
   </body>
-=======
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-
-<div class="container mt-3">
-  <h2> TOLONGA TEMPIKNYA </h2>
-  <br>            
-  <a href="data_murid/tambah" class="btn btn-primary">Tambah Data</a>
-
-  <table class="table table-bordered" name="tabelmurid">
-    <thead>
-        <tr>
-            <th>Absen</th>
-            <th>NIS</th>
-            <th>Gender</th>
-            <th>Nama</th>
-            <th>Kelas</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-  <?php if(!empty($model["dataMurid"])) : ?>
-            <?php foreach( $model["dataMurid"] as $murid) : ?>
-                <tr>
-                    <td><?= $murid['absen'] ?></td>
-                    <td><?= $murid['noinduk']?></td>
-                    <td><?= $murid['gender']?></td>
-                    <td><?= $murid['nama']  ?></td>
-                    <td><?= $murid['kelas'] ?></td>
-                    <td>
-                    <a href="data_murid/edit/<?= $murid['noinduk'] ?>">Edit</a>
-                    <a href="/admin/data_murid/hapus/<?= $murid['noinduk'] ?>" onclick="konfirmasiHapus()">Hapus</a>
-                    <a href="/admin/data_murid/melanggar/<?= $murid['noinduk'] ?>">Melanggar</a>
-                    </td>
-                    
-                </tr>
-            <?php endforeach; ?>
-     <?php endif; ?>
-    </tbody>
-</table>
-
-
-</div>
-
-</body>
->>>>>>> 7b7cc68 (api error dikit)
 </html>
