e4c7b81b6dbb99135e6adc90963733cc4fdc45d1
Kurniawan <fajard916@gmail.com>
2023-12-05 08:21:01 +0700

    Update from murid melanggar

--- app/View/admin/pelanggaran/tambah_pelanggaran.php
+++ app/View/admin/pelanggaran/tambah_pelanggaran.php
@@ -1,13 +1,28 @@
-<form class="col-md-6" action="/admin/tambahpelanggaran" method="post">
-  <div class="mb-3">
-    <label for="nama" class="form-label">Nama : </label>
-    <input type="text" class="form-control" id="nama" name="nama">
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="card card-primary my-5">
+        <div class="card-header bg-success">
+          <h3 class="card-title text-light">Form Tambah Data Murid</h3>
+        </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form class="quickForm" action="/admin/tambahpelanggaran" method="post">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="nama" class="form-label">Nama  </label>
+              <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+            <div class="form-group my-4">
+              <label for="nilai" class="form-label">Nilai Bobot  </label>
+              <input type="text" class="form-control" id="nilai" name="nilai">
+            </div>
+          </div>
+          <div class="card-footer">
+          <button type="submit" class="btn btn-success">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
-  <div class="mb-3">
-    <label for="nilai" class="form-label">Nilai Bobot : </label>
-    <input type="text" class="form-control" id="nilai" name="nilai">
-  </div>
-  <div class="form-check mb-3">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</div>
